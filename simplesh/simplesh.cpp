ccc8625107daa6b20f2d7cb0f1713825dc23aec6
qradimir <qradimir@yandex.ru>
2016-05-31 12:04:09 +0300

    errors handled; defended

--- simplesh/simplesh.cpp
+++ simplesh/simplesh.cpp
@@ -17,8 +17,12 @@ using namespace std;
 void write_all(int fd, const char *buf, size_t len);
 void write_all(int fd, const string &str);
 
+vector<pid_t> children{};
+
 inline void check_error() {
-	if (errno != EINTR) exit(errno);
+	if (errno != EINTR) {
+		exit(errno);
+	}
 }
 
 inline void check_error(size_t res) {
@@ -36,6 +40,7 @@ void check_error(ssize_t res, string const& error_str) {
 	if (res == -1) check_error(error_str);	
 }
 
+
 vector<string> &split(const string &s, char delim, vector<string> &elems) {
 	stringstream ss(s);
 	string item;
@@ -45,10 +50,9 @@ vector<string> &split(const string &s, char delim, vector<string> &elems) {
 	return elems;
 }
 
-vector<pid_t> children{};
 int* firstfd;
 
-char **get_args(string command) {
+char **get_args(const string &command) {
 	vector<string> words;
 	split(command, ' ', words);
 	
@@ -61,41 +65,66 @@ char **get_args(string command) {
 	return args;
 }
 
+bool try_close(int fd) {
+	while (close(fd) == -1) {
+		if (errno != EINTR) 
+			return false;
+	}
+	return true;
+}
+
+void try_duplicate(int fd1, int fd2) {
+	while (dup2(fd1, fd2) == -1) {
+		check_error();
+	}
+}
+
 void close_pipe(int *pipefd) {
-	close(pipefd[0]);
-	close(pipefd[1]);
+	try_close(pipefd[0]);
+	try_close(pipefd[1]);
 	delete [] pipefd;
 }
 
-void exec_command(string const &command, int *infd, int *outfd) {
+bool exec_command(const string &command, int *infd, int *outfd) {
 	char **args = get_args(command);
 	pid_t cpid = fork();
+	if (cpid == -1) return false;
 	if (cpid == 0) {
-		dup2(infd[0], STDIN_FILENO);
-		if (outfd != NULL) dup2(outfd[1], STDOUT_FILENO);
+		try_duplicate(infd[0], STDIN_FILENO);
+		if (outfd != NULL) try_duplicate(outfd[1], STDOUT_FILENO);
 		if (infd != firstfd) close_pipe(infd);
 		if (outfd != NULL) close_pipe(outfd);
 		close_pipe(firstfd);
-		check_error(execvp(args[0], const_cast<char* const*>(args)), "execution a command \'" + string{args[0]} + "\'");
+		(execvp(args[0], const_cast<char* const*>(args)), "execution a command \'" + string{args[0]} + "\'");
 	}
 	children.push_back(cpid);
+	return true;
 }
 
-void exec_commandpipe(vector<string> cpipe) {
+bool exec_commandpipe(const vector<string> &cpipe) {
 	int *infd = new int[2], *outfd = new int[2];
 	firstfd = infd;
-	pipe(infd);
-	pipe(outfd);
+	if (pipe(infd) == -1) return false;
+
 	for (int i = 0; i < cpipe.size() - 1; ++i) {
-		pipe(outfd);
-		exec_command(cpipe[i], infd, outfd);
-		
+		if (pipe(outfd) == -1) {
+			if (firstfd != infd) close_pipe(infd);
+			close_pipe(firstfd);
+			return false;
+		}
+		if (!exec_command(cpipe[i], infd, outfd)) {
+			if (firstfd != infd) close_pipe(infd);
+			close_pipe(firstfd);
+			close_pipe(outfd);
+			return false;
+		}
 		if (firstfd != infd) close_pipe(infd);
 		infd = outfd;
 		outfd = new int[2];
 	}
-	exec_command(cpipe[cpipe.size() - 1], infd, NULL);
+	bool success = exec_command(cpipe[cpipe.size() - 1], infd, NULL);
 	if (firstfd != infd) close_pipe(infd);
+	return success;
 }
 
 void write_all(int fd, const char *buf, size_t len) {
@@ -120,6 +149,14 @@ bool sig_intr = false;
 void signal_handler(int signal, siginfo_t *siginfo, void *ptr) {
 	if (signal == SIGINT) sig_intr = true;
 	if (signal == SIGCHLD && siginfo->si_pid == children[0]) first_dead = true; 	
+}
+
+void check_sig_intr() {
+	if (sig_intr) {
+		for (pid_t child : children) 
+			kill(child, SIGINT);
+	}
+	sig_intr = false;
 }
 
 const string INV{"$ "};
@@ -168,46 +205,52 @@ int main() {
 
 		vector<string> subcommands{};
 		split(com, '|', subcommands);
-		exec_commandpipe(subcommands);
-		
-		write_all(firstfd[1], tail);
-		while(!sig_intr && !first_dead) {
-			ssize = read(STDIN_FILENO, buffer, BUF_CAPACITY);
-			if (ssize == -1) {
-				check_error("calling read");
-				continue;
-			}	
-			if (ssize == 0) {
-				close(firstfd[1]);
-				break;
+		bool success = exec_commandpipe(subcommands);
+		if (success) {
+			write_all(firstfd[1], tail);
+			while(!sig_intr && !first_dead) {
+				ssize = read(STDIN_FILENO, buffer, BUF_CAPACITY);
+				if (ssize == -1) {
+					check_error("calling read");
+					continue;
+				}	
+				if (ssize == 0) {
+					try_close(firstfd[1]);
+					break;
+				}
+	
+				write_all(firstfd[1], buffer, (size_t) ssize);
 			}
+		}
 
-			write_all(firstfd[1], buffer, (size_t) ssize);
-		}
-		if (sig_intr) {
-			for (pid_t child: children) 
-				kill(child, SIGINT);		
-		}
+		check_sig_intr();
 
 		for (pid_t child : children) {
-			waitpid(child, NULL, 0);
+			while (true) {
+				if (waitpid(child, NULL, 0) == -1) {
+					check_sig_intr();
+					continue;
+				}
+				break;
+			}
 		}
 
 		children.clear();
 		checked_symbols = 0;
 		command.clear();
-	
-		close(firstfd[1]);
-		while ((ssize = read(firstfd[0], buffer, BUF_CAPACITY)) != 0) {
-			if (ssize == -1) {
-				check_error("calling read");
-				continue;
-			}
-			command += string{buffer, (size_t) ssize};
-		}
-		close(firstfd[0]);
-		delete [] firstfd;
 
+		if (success) {	
+			try_close(firstfd[1]);
+			while ((ssize = read(firstfd[0], buffer, BUF_CAPACITY)) != 0) {
+				if (ssize == -1) {
+					check_error("calling read");
+					continue;
+				}
+				command += string{buffer, (size_t) ssize};
+			}
+			try_close(firstfd[0]);
+			delete [] firstfd;
+		}
 		sig_intr = false;
 		first_dead = false;
 
